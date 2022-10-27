a9ff2b9fa5be4d3779ee778c73868493195fc9c3
yourusername <nick_gayem@yahoo.com>
2022-10-26 19:50:11 -0700

    Fixed ChatViewcontroller UI

--- Flash Chat IOS app/AppDelegate.swift
+++ Flash Chat IOS app/AppDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 import FirebaseCore
 import FirebaseFirestore
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,8 +20,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         FirebaseApp.configure()
         
         let db = Firestore.firestore()
+        print(db)
         
-        
+//        IQKeyboardManager.shared.enable = true
+//        IQKeyboardManager.shared.enableAutoToolbar = false
+//        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
         return true
     }
 
